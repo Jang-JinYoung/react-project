3746d20cba7fec4197e8a2609d0269ff4ab8291b
장진영 <jinyoung_8965@naver.com>
2022-06-29 15:25:03 +0900

    commit

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const MainPage = () => {
   return (
     <div className="">
-      <p style={{color: "red"}}><b>지현아 사랑해!!!!!!!!!!!!!!!!!!</b></p>
+      <p style={{color: "red"}}>지현아 사랑해!!!!!!!!!!!!!!!!!!</p>
       <p>지현이 최고야!!!!!!!!!!!!!!!!!!</p>
     </div>
   )
